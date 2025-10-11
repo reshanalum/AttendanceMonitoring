b2fad3b70c028e288cf413fb7446879b5f789c9e
Reshan <reshanaalum12345@gmail.com>
2025-10-11 20:12:44 +0800

    Modified Configuration version 1.1

---
+++ AttendanceMonitoring/Configurations/UserConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using AttendanceMonitoring.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceMonitoring.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable(nameof(User));
+            builder.HasKey(c => c.UserId);
+            builder.Property(c => c.FirstName)
+                .HasColumnType("varchar(150)");
+            builder.Property(c => c.LastName)
+                .HasColumnType("varchar(150)");
+            builder.Property(c => c.PhoneNumber)
+                .HasColumnType("varchar(11)");
+            builder.Property(c => c.Email)
+                .HasColumnType("varchar(150)");
+            builder.Property(c => c.IsAdmin)
+                .HasColumnType("bit");
+        }
+    }
+}
