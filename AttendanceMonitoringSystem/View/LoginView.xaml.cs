a93cafb82e0d00b3945ac69640ce85b4178851b6
Fergievon Teves <leiasonata24@gmail.com>
2025-11-01 22:20:05 +0800

    Login View File

--- AttendanceMonitoringSystem/View/LoginView.xaml.cs
+++ AttendanceMonitoringSystem/View/LoginView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace AttendanceMonitoringSystem.View
 {
     /// <summary>
-    /// Interaction logic for Page1.xaml
+    /// Interaction logic for LoginView.xaml
     /// </summary>
-    public partial class Page1 : Page
+    public partial class LoginView : Window
     {
-        public Page1()
+        public LoginView()
         {
             InitializeComponent();
         }
