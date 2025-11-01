e1f04b432461dab7fa0456b45a26b10199d6dbf5
Fergievon Teves <leiasonata24@gmail.com>
2025-11-01 22:02:45 +0800

    Comman,View,ViewModel

---
+++ AttendanceMonitoring/View/PCLoginView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using AttendanceMonitoring.ViewModel;
+
+namespace TEVES_FP_WPF_PCSHOP.View
+{
+    /// <summary>
+    /// Interaction logic for PCLoginView.xaml
+    /// </summary>
+    public partial class PCLoginView : Window
+    {
+        public PCLoginView()
+        {
+            InitializeComponent();
+            DateContext = new LoginViewModel();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void ButtonMinimize_Click(object sender, RoutedEventArgs e) 
+        { 
+            WindowState = WindowState.Minimized;
+        }
+        private void ButtonClose_Click(object sender, RoutedEventArgs e) 
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
