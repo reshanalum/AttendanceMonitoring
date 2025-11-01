73f8fb92581db4cf2db136c42e7d02b69478980b
Fergievon Teves <leiasonata24@gmail.com>
2025-11-01 22:12:34 +0800

    Finals Commit

--- AttendanceMonitoringSystem/Command/RelayCommands.cs
+++ AttendanceMonitoringSystem/Command/RelayCommands.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace AttendanceMonitoring.Command
+namespace AttendanceMonitoringSystem.Command
 {
     class RelayCommands : ICommand
     {
