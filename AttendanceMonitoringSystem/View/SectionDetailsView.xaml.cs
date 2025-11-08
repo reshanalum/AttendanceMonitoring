a862319ead86e5fa59411bc8b527754ecd779acd
Reshan <reshanaalum12345@gmail.com>
2025-11-08 18:43:42 +0800

    Section list and Section details UI added

---
+++ AttendanceMonitoringSystem/View/SectionDetailsView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AttendanceMonitoringSystem.View
+{
+    /// <summary>
+    /// Interaction logic for SectionDetailsView.xaml
+    /// </summary>
+    public partial class SectionDetailsView : UserControl
+    {
+        public SectionDetailsView()
+        {
+            InitializeComponent();
+        }
+
+        private void StudentButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AttendanceHistoryButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+    }
+
+
+
+}
---
+++ AttendanceMonitoringSystem/View/SectionListView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AttendanceMonitoringSystem.View
+{
+    /// <summary>
+    /// Interaction logic for SectionListView.xaml
+    /// </summary>
+    public partial class SectionListView : UserControl
+    {
+        public SectionListView()
+        {
+            InitializeComponent();
+        }
+
+        private void SectionListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+
+
+}
