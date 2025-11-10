95cfa661930c1bb48c93dc2a04ccd653269b0ec3
Reshan <reshanaalum12345@gmail.com>
2025-11-10 21:00:59 +0800

    Attendance Monitoring database added as a project reference to the wpf

--- AttendanceMonitoringSystem/ViewModel/StudentListVM.cs
+++ AttendanceMonitoringSystem/ViewModel/StudentListVM.cs
@@ -1,51 +1,135 @@
-﻿using AttendanceMonitoringSystem.View;
+﻿using AttendanceMonitoringSystem.Commands;
+using AttendanceMonitoringSystem.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
-using AttendanceMonitoringSystem.Commands;
+using AttendanceMonitoring.Models;
 
 namespace AttendanceMonitoringSystem.ViewModel
 {
     public class StudentListVM: NotifyPropertyChanged
     {
-
+        private readonly DashboardVM _dashboardVM;
         public ICommand ShowEditStudentCommand { get; set; }
         public ICommand ShowAddStudentCommand { get; set; }
 
-        private object _currentView;
-        public object CurrentView
+        private Employee selectedEmployee;
+
+        public Employee SelectedEmployee
         {
-            get { return _currentView; }
+            get => selectedEmployee;
             set
             {
-                _currentView = value;
-                OnPropertyChanged(nameof(CurrentView));
+                selectedEmployee = value;
+                OnPropertyChanged();
             }
         }
 
-        public StudentListVM()
+        private string employeeSearchText;
+
+        public string EmployeeSearchText
+        {
+            get => employeeSearchText;
+            set
+            {
+                employeeSearchText = value;
+                FilterEmployees();
+            }
+        }
+
+        private void LoadEmployees()
+        {
+            using var context = new GardenGloryContext();
+            var employees = context.Employees.ToList();
+
+            EmployeesList.Clear();
+            foreach (var emp in employees)
+            {
+                EmployeesList.Add(emp);
+            }
+        }
+
+        public StudentListVM(DashboardVM _dashboardVM)
         {
 
         }
 
-        public void ExecuteShowSectionList()
+        public void DeleteEmployee(object obj)
         {
 
-            var sectionListView = new SectionListView();
-            CurrentView = sectionListView;
+            if (obj is not Employee employeeToDelete)
+            {
+                MessageBox.Show("No employee selected.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var result = MessageBox.Show(
+                $"Are you sure you want to delete employee: {employeeToDelete.FirstName} {employeeToDelete.LastName}?",
+                "Confirm Deletion", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+            if (result != MessageBoxResult.Yes)
+                return;
+
+            using var context = new GardenGloryContext();
+            var employeeInDb = context.Employees.FirstOrDefault(c => c.EmployeeId == employeeToDelete.EmployeeId);
+
+            if (employeeInDb == null)
+            {
+                MessageBox.Show("The selected employee does not exist in the database.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            context.Employees.Remove(employeeInDb);
+            context.SaveChanges();
+            EmployeesList.Remove(employeeToDelete);
+            SelectedEmployee = null;
         }
 
-        public void ExecuteShowDashboard()
+
+
+        private void ExecuteEditEmployeeCommand(object obj)
         {
-
-            //var employeesView = new Employees(this, Permissions);
-            //CurrentView = employeesView;
-            //Caption = "Employee";
+            if (obj is Employee employee)
+            {
+                SelectedEmployee = employee;
+                var editView = new EditEmployee();
+                editView.DataContext = new EditEmployeeVM(employee, _mainVM);
+                _mainVM.CurrentView = editView;
+            }
         }
 
+        private void ExecuteAddEmployeeCommand(object obj)
+        {
+            var addView = new AddEmployee();
+            addView.DataContext = new AddEmployeeVM(_mainVM);
+            _mainVM.CurrentView = addView;
+        }
+
+        public void FilterEmployees()
+        {
+            string search = EmployeeSearchText.ToLower();
+
+            using var context = new GardenGloryContext();
+            var employees = context.Employees
+                .Where(c =>
+                    c.EmployeeId.ToString().Contains(search) ||
+                    c.FirstName.ToLower().Contains(search) ||
+                    c.LastName.ToLower().Contains(search) ||
+                    c.CellPhone.ToLower().Contains(search) ||
+                    c.ExperienceLevel.ToLower().Contains(search))
+                .ToList();
+
+            EmployeesList.Clear();
+            foreach (var emp in employees)
+            {
+                EmployeesList.Add(emp);
+            }
+
+        }
 
     }
 }
