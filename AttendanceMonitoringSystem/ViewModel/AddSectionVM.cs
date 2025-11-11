1235a7d5d67bdb5d848c629041545d4c0ac0a4f3
Fergievon Teves <leiasonata24@gmail.com>
2025-11-11 23:08:18 +0800

    Section Page Update 1

--- AttendanceMonitoringSystem/ViewModel/AddSectionVM.cs
+++ AttendanceMonitoringSystem/ViewModel/AddSectionVM.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using AttendanceMonitoring.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AttendanceMonitoringSystem.ViewModel
 {
-    internal class AddSectionVM
+    public class AddSectionVM
     {
+        private readonly DashboardVM _dashboardVM;
+        public ObservableCollection<Student> StudentList { get; set; } = new ObservableCollection<Student>();
+        public ObservableCollection<Class_Adviser> TeacherList { get; set; } = new ObservableCollection<Class_Adviser>();
+        public string NewSectionName { get; set; }
+
+        public AddSectionVM(DashboardVM dashboardVM)
+        {
+            _dashboardVM = dashboardVM;
+
+            LoadStudents();
+            LoadTeachers();
+        }
+
+        private void LoadTeachers()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadStudents()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
