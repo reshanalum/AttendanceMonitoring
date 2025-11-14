99fa758984ec54face5dabc240ef52216f47b174
Reshan <reshanaalum12345@gmail.com>
2025-11-14 18:16:23 +0800

    Pagination Deleted

--- AttendanceMonitoring/AttendanceMonitoringContext.cs
+++ AttendanceMonitoring/AttendanceMonitoringContext.cs
@@ -26,9 +26,8 @@ namespace AttendanceMonitoring
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseSqlite("Data Source=C:\\Users\\resha\\Documents\\AttendanceMonitoringFINAL.db");
-            //C:\Users\15-FB1019AX r5\OneDrive\Documents\Fergie Codes\AttendanceMonitoring\AttendanceMonitoring-master
-            optionsBuilder.UseSqlite("Data Source=C:\\Users\\15-FB1019AX r5\\OneDrive\\Documents\\Fergie Codes\\AttendanceMonitoring\\AttendanceMonitoring-master.db");
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\resha\\Documents\\AttendanceMonitoringFINALPROMISE.db");
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
